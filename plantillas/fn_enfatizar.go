e7e2853e3b55a34df93c9cd621a7ba3159c2fbc8
Andrew <andrew@bce2>
2025-02-23 00:18:48 -0600

    Usar backticks para enfatizar como código en html

--- plantillas/fn_enfatizar.go
+++ plantillas/fn_enfatizar.go
@@ -9,6 +9,7 @@ import (
 var (
 	reBold   = regexp.MustCompile(`\*(.*?)\*`)
 	reItalic = regexp.MustCompile(`_(.*?)_`)
+	reCode   = regexp.MustCompile("`(.*?)`")
 )
 
 // Enfatizar busca texto entre asteriscos y lo convierte en negritas,
@@ -20,6 +21,9 @@ func Enfatizar(text string) template.HTML {
 	})
 	escapedText = reItalic.ReplaceAllStringFunc(escapedText, func(match string) string {
 		return "<em>" + strings.Trim(match, "_") + "</em>"
+	})
+	escapedText = reCode.ReplaceAllStringFunc(escapedText, func(match string) string {
+		return "<code>" + strings.Trim(match, "`") + "</code>"
 	})
 	return template.HTML(escapedText)
 }
