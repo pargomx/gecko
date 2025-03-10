87f84410020a3e6040cbb9680710030881c179c5
Andrew <andrew@bce2>
2025-03-10 02:15:25 -0600

    Soportar links con sintaxis de markdown en enfatizar

--- plantillas/fn_enfatizar.go
+++ plantillas/fn_enfatizar.go
@@ -2,14 +2,18 @@ package plantillas
 
 import (
 	"html/template"
+	"net/url"
 	"regexp"
 	"strings"
+
+	"github.com/pargomx/gecko/gkt"
 )
 
 var (
 	reBold   = regexp.MustCompile(`\*(.*?)\*`)
 	reItalic = regexp.MustCompile(`_(.*?)_`)
 	reCode   = regexp.MustCompile("`(.*?)`")
+	reLink   = regexp.MustCompile(`\[(.*?)\]\((.*?)\)`) // Link markdown [title](https://example.com)
 )
 
 // Enfatizar busca texto entre asteriscos y lo convierte en negritas,
@@ -24,6 +28,29 @@ func Enfatizar(text string) template.HTML {
 	})
 	escapedText = reCode.ReplaceAllStringFunc(escapedText, func(match string) string {
 		return "<code>" + strings.Trim(match, "`") + "</code>"
+	})
+	escapedText = reLink.ReplaceAllStringFunc(escapedText, func(match string) string {
+		matches := reLink.FindStringSubmatch(match)
+		if len(matches) == 3 {
+			desc := matches[1]
+			unsafeLink := gkt.SinEspaciosNinguno(matches[2])
+			if unsafeLink == "" {
+				return `<a href="#bad_url" class="hover:underline">` + desc + ` [undefined link]</a>`
+			}
+			URL, err := url.Parse(unsafeLink)
+			if err != nil {
+				return `<a href="#bad_url" class="hover:underline">` + desc + " [bad link: " + err.Error() + `]</a>`
+			}
+			// Si no es absoluta del mismo origen entonces debe llevar https o http.
+			if !strings.HasPrefix(unsafeLink, "/") && URL.Scheme != "http" && URL.Scheme != "https" {
+				URL.Scheme = "https"
+			}
+			if desc == "" {
+				desc = URL.String()
+			}
+			return `<a href="` + URL.String() + `" class="hover:underline">` + desc + `</a>`
+		}
+		return match
 	})
 	return template.HTML(escapedText)
 }
